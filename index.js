f4817059f74a41c79e4cbfe3bb141646f50f1f17
Umar Siddiqui <umarsiddiqui64@gmail.com>
2024-12-03 22:22:25 +0530

    Added solution for managing a to-do list

---
+++ index.js
@@ -0,0 +1,9 @@
+let toDoList = ["Wake up", "Brush teeth", "Have breakfast", "Attend meeting", "Exercise"];
+
+toDoList.unshift("Check emails"); 
+console.log("To-Do List after adding a new task:", toDoList);
+
+toDoList.pop(); 
+console.log("To-Do List after removing the last task:", toDoList);
+
+console.log("Updated To-Do List:", toDoList);
